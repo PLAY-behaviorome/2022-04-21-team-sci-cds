c1a9eaf6171919b782101d698142a9bd40ad8b6f
Rick Gilmore <rog1@psu.edu>
2022-04-08 15:47:40 -0400

    More fleshing out.

--- R/make_talks.R
+++ R/make_talks.R
@@ -1,4 +1,5 @@
-# Render talks
-
-rmarkdown::render("play-overview.Rmd")
-rmarkdown::render("data-management.Rmd")
+make_talks <- function(db_account = "email@university.edu") {
+  rmarkdown::render("play-overview.Rmd", params=list(db_login = db_account))
+  rmarkdown::render("data-management.Rmd")
+  databraryapi::logout_db()
+}
